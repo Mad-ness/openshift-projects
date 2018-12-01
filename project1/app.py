d110ab50342924379da600df69b1996471236b79
Dmitrii Mostovshchikov <dmadm2008@gmail.com>
2018-12-01 21:53:06 +0100

    Added /whoareyou call

--- project1/app.py
+++ project1/app.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import platform
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -8,6 +10,9 @@ def ping_pong(): return "pong"
 
 @app.route('/health')
 def health_check(): return "ok"
+
+@app.route('/whoareyou')
+def whoareyou(): return platform.node()
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8080)
